03a0fa052185b968b72651b345f031debe8116a8
Glen Harmon <ghar@nnit.com>
2016-07-13 09:50:59 +0200

    Git config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,35 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+SCRIPT=$(readlink -f "$0")
+DOTFILESPATH=$(dirname "$SCRIPT" | sed "s/\/c/c:/" | sed "s/\//\\\/g")
 
 git pull origin master;
 
-function doIt() {
+function bootstrap() {
+    if [ -z "$OS" ]; then
+        linuxBootstrap;
+    else
+        if [ "$OS" = "Windows_NT" ]; then
+            windowsBootstrap;
+        else
+            echo "ERROR: Unknown OS: $OS"
+        fi;
+    fi;
+}
+
+function windowsBootstrap() {
+    dotfiles=( ".aliases" ".bash_profile" ".bash_prompt" ".bashrc" ".gitignore" ".gitmodules" ".gitconfig" ".inputrc" ".vimrc" ".wgetrc" ".vim")
+
+    for i in "${dotfiles[@]}"
+    do
+        DOTFILENAME=$(echo "$DOTFILESPATH\\$i")
+        cp -r "$DOTFILENAME" ~
+    done
+    unset DOTFILENAME
+    mv ~/.vimrc ~/_vimrc    
+}
+
+function linuxBootstrap() {
     rsync --exclude ".git/" \
         --exclude ".DS_Store" \
         --exclude "bootstrap.sh" \
@@ -15,12 +40,17 @@ function doIt() {
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt;
+    bootstrap;
 else
     read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt;
+        bootstrap;
     fi;
 fi;
-unset doIt;
+
+unset bootstrap;
+unset windowsBootstrap;
+unset linuxBootstrap;
+unset SCRIPT;
+unset DOTFILESPATH;
