8906698f351c7228f602fa7343f4a4191c5a5dfe
Glen Harmon <glencharmon@gmail.com>
2016-07-20 14:31:55 +0200

    #bug

--- bootstrap.sh
+++ bootstrap.sh
@@ -43,7 +43,6 @@ if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	bootstrap;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		bootstrap;
 	fi;
