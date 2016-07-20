8f1220a67c3c35f77cd78c3783d042f113896f10
Glen Harmon <glencharmon@gmail.com>
2016-07-20 15:30:12 +0200

    Removal of trailing ;

--- brew.sh
+++ brew.sh
@@ -28,15 +28,15 @@ brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells
+  chsh -s /usr/local/bin/bash
+fi
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
 
 # Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
+# Note that the order in which these are installed is important
 # see http://git.io/brew-narwhal-ringo.
 brew install ringojs
 brew install narwhal
