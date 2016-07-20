43ef29db1f975a0faf0fd325785419f792be9c8c
Glen Harmon <glencharmon@gmail.com>
2016-07-20 14:30:04 +0200

    readlink bug

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,52 +1,52 @@
 #!/usr/bin/env bash
 
-SCRIPT=$(readlink -f "$0")
+SCRIPT=$(readlink -f -- $0")
 DOTFILESPATH=$(dirname "$SCRIPT" | sed "s/\/c/c:/" | sed "s/\//\\\/g")
 
 git pull origin master;
 
 function bootstrap() {
-    if [ -z "$OS" ]; then
-        linuxBootstrap;
-    else
-        if [ "$OS" = "Windows_NT" ]; then
-            windowsBootstrap;
-        else
-            echo "ERROR: Unknown OS: $OS"
-        fi;
-    fi;
+	if [ -z "$OS" ]; then
+		linuxBootstrap;
+	else
+		if [ "$OS" = "Windows_NT" ]; then
+			windowsBootstrap;
+		else
+			echo "ERROR: Unknown OS: $OS"
+		fi;
+	fi;
 }
 
 function windowsBootstrap() {
-    dotfiles=( ".aliases" ".bash_profile" ".function" ".bash_prompt" ".bashrc" ".gitignore" ".gitmodules" ".gitconfig" ".inputrc" ".vimrc" ".wgetrc" ".vim")
+	dotfiles=( ".aliases" ".bash_profile" ".function" ".bash_prompt" ".bashrc" ".gitignore" ".gitmodules" ".gitconfig" ".inputrc" ".vimrc" ".wgetrc" ".vim")
 
-    for i in "${dotfiles[@]}"
-    do
-        DOTFILENAME=$(echo "$DOTFILESPATH\\$i")
-        cp -r "$DOTFILENAME" ~
-    done
-    unset DOTFILENAME
-    mv ~/.vimrc ~/_vimrc
+	for i in "${dotfiles[@]}"
+	do
+		DOTFILENAME=$(echo "$DOTFILESPATH\\$i")
+		cp -r "$DOTFILENAME" ~
+	done
+	unset DOTFILENAME
+	mv ~/.vimrc ~/_vimrc
 }
 
 function linuxBootstrap() {
-    rsync --exclude ".git/" \
-        --exclude ".DS_Store" \
-        --exclude "bootstrap.sh" \
-        --exclude "README.md" \
-        --exclude "LICENSE-MIT.txt" \
-        -avh --no-perms . ~;
-    source ~/.bash_profile;
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    bootstrap;
+	bootstrap;
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-    echo "";
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        bootstrap;
-    fi;
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		bootstrap;
+	fi;
 fi;
 
 unset bootstrap;
