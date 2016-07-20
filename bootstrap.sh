1de45e9a23bd24214b465a33be844c65f49b26d1
Glen Harmon <glencharmon@gmail.com>
2016-07-20 16:33:22 +0200

    bugs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 function start_bootstrap() {
 	if [ "$OS" == "" ]; then
 		linuxBootstrap
@@ -14,6 +13,7 @@ function start_bootstrap() {
 function windowsBootstrap() {
 	local DOTFILESPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 	git --git-dir=$DOTFILESPATH\.git pull origin master
+
 	DOTFILESPATH=$(echo "$DOTFILESPATH" | sed "s/\/c/c:/" | sed "s/\//\\\/g")
 	dotfiles=( ".aliases" ".bash_profile" ".function" ".bash_prompt" ".bashrc" ".gitignore" ".gitmodules" ".gitconfig" ".inputrc" ".vimrc" ".wgetrc" ".vim" )
 
@@ -26,15 +26,25 @@ function windowsBootstrap() {
 }
 
 function linuxBootstrap() {
-	local DOTFILESPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	# Get bootstrap.sh directory path
+	local SOURCE="${BASH_SOURCE[0]}"
+	while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	  SOURCE="$(readlink "$SOURCE")"
+	  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	done
+	DOTFILESPATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
+	# Update
 	git --git-dir=$DOTFILESPATH/.git pull origin master
+
+	# Sync
 	rsync --exclude '.git/' \
 		--exclude '.DS_Store' \
 		--exclude 'bootstrap.sh' \
 		--exclude 'README.md' \
 		--exclude 'LICENSE-MIT.txt' \
-		-avh --no-perms $DOTFILESPATH ~
+		-avh --no-perms $DOTFILESPATH $HOME
 	source ~/.bash_profile
 }
 
